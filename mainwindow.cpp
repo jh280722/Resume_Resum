4fa2e11c54d48e38949f5d0622018c627c8d7451
junho <jh280722@naver.com>
2020-07-07 17:07:52 +0900

    DP-25 code 정리

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,17 +1,17 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
+MainWindow::MainWindow(QWidget* parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    num[0]=num[1]=num[2]=0;
-//    int w=ui->label_pic->width();
-//    int h=ui->label_pic->height();
-//    QPixmap pix(":/img/tmp.jpg");
-//    ui->label_pic->setPixmap(pix);
-//    ui->label_pic->setPixmap(pix.scaled(w,h,Qt::KeepAspectRatio));
+    num[0] = num[1] = num[2] = 0;
+    //    int w=ui->label_pic->width();
+    //    int h=ui->label_pic->height();
+    //    QPixmap pix(":/img/tmp.jpg");
+    //    ui->label_pic->setPixmap(pix);
+    //    ui->label_pic->setPixmap(pix.scaled(w,h,Qt::KeepAspectRatio));
 }
 
 MainWindow::~MainWindow()
@@ -23,19 +23,19 @@ MainWindow::~MainWindow()
 
 QPushButton* MainWindow::new_button(QString name, QString str)
 {
-       QPushButton* button = new QPushButton(str);
-       button->setObjectName(name);
-       return button;
+    QPushButton* button = new QPushButton(str);
+    button->setObjectName(name);
+    return button;
 }
 
-void MainWindow::make_tab(QWidget* Tab){
-    QWidget *widget = Tab;
-    QVBoxLayout *layout = new QVBoxLayout(widget);
+void MainWindow::make_tab(QWidget* Tab) {
+    QWidget* widget = Tab;
+    QVBoxLayout* layout = new QVBoxLayout(widget);
 
     widget->setLayout(layout);
 
-    QGroupBox *box = new QGroupBox(Kor("기본 정보"), widget);
-    QVBoxLayout *boxLayout = new QVBoxLayout(box);
+    QGroupBox* box = new QGroupBox(Kor("기본 정보"), widget);
+    QVBoxLayout* boxLayout = new QVBoxLayout(box);
     layout->addWidget(box);
 
     QWidget* nameWidget = new QWidget(box);
@@ -46,22 +46,22 @@ void MainWindow::make_tab(QWidget* Tab){
     boxLayout->addWidget(ageWidget);
     boxLayout->addWidget(addressWidget);
 
-    QHBoxLayout *nameLayout = new QHBoxLayout(nameWidget);
+    QHBoxLayout* nameLayout = new QHBoxLayout(nameWidget);
     nameLayout->addWidget(new QLabel(Kor("이 름 :")));
     nameLayout->addWidget(new QLineEdit(nameWidget));
 
 
-    QHBoxLayout *ageLayout = new QHBoxLayout(ageWidget);
+    QHBoxLayout* ageLayout = new QHBoxLayout(ageWidget);
     ageLayout->addWidget(new QLabel(Kor("나 이 :")));
     ageLayout->addWidget(new QLineEdit(ageWidget));
 
-    QHBoxLayout *addressLayout = new QHBoxLayout(addressWidget);
+    QHBoxLayout* addressLayout = new QHBoxLayout(addressWidget);
     addressLayout->addWidget(new QLabel(Kor("주 소 :")));
     addressLayout->addWidget(new QLineEdit(addressWidget));
 
 
     QWidget* validateWidget = new QWidget(widget);
-    QHBoxLayout *validateLayout = new QHBoxLayout(validateWidget);
+    QHBoxLayout* validateLayout = new QHBoxLayout(validateWidget);
     validateLayout->addWidget(new QPushButton(Kor("확 인"), validateWidget));
     validateLayout->addWidget(new QPushButton(Kor("리 셋"), validateWidget));
     validateLayout->addWidget(new QPushButton(Kor("취 소"), validateWidget));
@@ -69,70 +69,70 @@ void MainWindow::make_tab(QWidget* Tab){
 }
 
 
-void MainWindow::on_plus_clicked(){
-    QObject *sel=QObject::sender();
+void MainWindow::on_plus_clicked() {
+    QObject* sel = QObject::sender();
     QString name;
-    QWidget *menu=0;
-    if(sel==mb[0]){
-        idx=0;
-        name=Kor("기본 정보 ")+QString::number(num[idx]);
-        menu=ui->dropmenu0;
+    QWidget* menu = 0;
+    if (sel == mb[0]) {
+        TopicIdx = 0;
+        name = Kor("기본 정보 ") + QString::number(num[TopicIdx]);
+        menu = ui->dropmenu0;
     }
-    else if(sel==mb[1]){
-        idx=1;
-        name=Kor("인적 사항 ")+QString::number(num[idx]);
-        menu=ui->dropmenu1;
+    else if (sel == mb[1]) {
+        TopicIdx = 1;
+        name = Kor("인적 사항 ") + QString::number(num[TopicIdx]);
+        menu = ui->dropmenu1;
     }
-    else if(sel==mb[2]){
-        idx=2;
-        name=Kor("자 소 서 ")+QString::number(num[idx]);
-        menu=ui->dropmenu2;
+    else if (sel == mb[2]) {
+        TopicIdx = 2;
+        name = Kor("자 소 서 ") + QString::number(num[TopicIdx]);
+        menu = ui->dropmenu2;
     }
-    mb[idx]->setText(name);
-    disconnect(mb[idx], SIGNAL(clicked()), this, SLOT(on_plus_clicked()));
-    PBList[idx].push_back(new_button("dm"+QString::number(idx)+"_"+QString::number(num[idx]),Kor("항목추가")));
-    mb[idx]=PBList[idx][num[idx]];
-    menu->layout()->addWidget(PBList[idx][num[idx]]);
-    QWidget* new_tab= new QWidget;
+    mb[TopicIdx]->setText(name);
+    disconnect(mb[TopicIdx], SIGNAL(clicked()), this, SLOT(on_plus_clicked()));
+    PBList[TopicIdx].push_back(new_button("dm" + QString::number(TopicIdx) + "_" + QString::number(num[TopicIdx]), Kor("항목추가")));
+    mb[TopicIdx] = PBList[TopicIdx][num[TopicIdx]];
+    menu->layout()->addWidget(PBList[TopicIdx][num[TopicIdx]]);
+    QWidget* new_tab = new QWidget;
     make_tab(new_tab);
-    TList[idx].push_back(new_tab);
-    connect(PBList[idx][num[idx]], SIGNAL(clicked()), this, SLOT(on_plus_clicked()));
-    connect(PBList[idx][num[idx]++], SIGNAL(clicked()), this, SLOT(connect_subTopic()));
+    TList[TopicIdx].push_back(new_tab);
+    connect(PBList[TopicIdx][num[TopicIdx]], SIGNAL(clicked()), this, SLOT(on_plus_clicked()));
+    connect(PBList[TopicIdx][num[TopicIdx]++], SIGNAL(clicked()), this, SLOT(connect_subTopic()));
 }
 
-void MainWindow::hide_show(int idx, int &flg){
-    QWidget* menu=0;
-    if(idx==0)
-        menu=ui->dropmenu0;
-    else if(idx==1)
-        menu=ui->dropmenu1;
-    else if(idx==2)
-        menu=ui->dropmenu2;
+void MainWindow::hide_show(int TopicIdx, int& flg) {
+    QWidget* menu = 0;
+    if (TopicIdx == 0)
+        menu = ui->dropmenu0;
+    else if (TopicIdx == 1)
+        menu = ui->dropmenu1;
+    else if (TopicIdx == 2)
+        menu = ui->dropmenu2;
 
-    if(flg==0){
-        flg=2;
-        PBList[idx].push_back(new_button("dm"+QString::number(idx)+"_"+QString::number(num[idx]),
-                                         Kor("항목추가")));
-        mb[idx]=PBList[idx][num[idx]];
-        menu->layout()->addWidget(PBList[idx][num[idx]]);
+    if (flg == 0) {
+        flg = 2;
+        PBList[TopicIdx].push_back(new_button("dm" + QString::number(TopicIdx) + "_" + QString::number(num[TopicIdx]),
+            Kor("항목추가")));
+        mb[TopicIdx] = PBList[TopicIdx][num[TopicIdx]];
+        menu->layout()->addWidget(PBList[TopicIdx][num[TopicIdx]]);
 
-        connect(PBList[idx][num[idx]], SIGNAL(clicked()), this, SLOT(on_plus_clicked()));
-        connect(PBList[idx][num[idx]++], SIGNAL(clicked()), this, SLOT(connect_subTopic()));
+        connect(PBList[TopicIdx][num[TopicIdx]], SIGNAL(clicked()), this, SLOT(on_plus_clicked()));
+        connect(PBList[TopicIdx][num[TopicIdx]++], SIGNAL(clicked()), this, SLOT(connect_subTopic()));
         ui->Tb1->setTabsClosable(true);
-        connect(ui->Tb1,SIGNAL(tabCloseRequested(int)),this,SLOT(on_Tb1_deleteTab(int)));
+        connect(ui->Tb1, SIGNAL(tabCloseRequested(int)), this, SLOT(on_Tb1_deleteTab(int)));
     }
-    else if(flg==1){
-        flg=2;
-        for(int i = 0; i < num[idx]; i++)
+    else if (flg == 1) {
+        flg = 2;
+        for (int i = 0; i < num[TopicIdx]; i++)
         {
-             PBList[idx][i]->show();
+            PBList[TopicIdx][i]->show();
         }
     }
-    else if(flg==2){
-        flg=1;
-        for(int i = 0; i < num[idx]; i++)
+    else if (flg == 2) {
+        flg = 1;
+        for (int i = 0; i < num[TopicIdx]; i++)
         {
-             PBList[idx][i]->hide();
+            PBList[TopicIdx][i]->hide();
         }
     }
 }
@@ -174,7 +174,6 @@ void MainWindow::connect_subTopic() {
 
     if (existList) {
         ui->Tb1->setCurrentIndex(tmpIdx);
-
     }
     else {
         mbt = PBList[TopicIdx][subTopicIdx];
@@ -185,42 +184,42 @@ void MainWindow::connect_subTopic() {
 
 }
 
-void MainWindow::on_Tb1_deleteTab(int idx){
-   ui->Tb1->removeTab(idx);
+void MainWindow::on_Tb1_deleteTab(int idx) {
+    ui->Tb1->removeTab(idx);
 
-//   disconnect(mbt,SIGNAL(clicked()),this,SLOT(connect_subTopic()));
-//   disconnect( mbt,SIGNAL(clicked()),this,SLOT(on_Tb1_addTab()));
+    //   disconnect(mbt,SIGNAL(clicked()),this,SLOT(connect_subTopic()));
+    //   disconnect( mbt,SIGNAL(clicked()),this,SLOT(on_Tb1_addTab()));
 }
-void MainWindow::on_Tb1_addTab(int subTopicIdx){
-    QWidget* new_tab= TList[idx][subTopicIdx];
-    ui->Tb1->addTab(new_tab,mbt->text());
+void MainWindow::on_Tb1_addTab(int subTopicIdx) {
+    QWidget* new_tab = TList[TopicIdx][subTopicIdx];
+    ui->Tb1->addTab(new_tab, mbt->text());
     //ui->Tb1->show();
 
-    if(ui->Tb1->indexOf(new_tab)!=-1){
-       // disconnect( mbt,SIGNAL(clicked()),this,SLOT(on_Tb1_addTab()));
-       // connect( mbt,SIGNAL(clicked()),this,SLOT(on_Tb1_addTab()));
-        //ui->Tb1->removeTab(ui->Tb1->indexOf(new_tab));
+    if (ui->Tb1->indexOf(new_tab) != -1) {
+        // disconnect( mbt,SIGNAL(clicked()),this,SLOT(on_Tb1_addTab()));
+        // connect( mbt,SIGNAL(clicked()),this,SLOT(on_Tb1_addTab()));
+         //ui->Tb1->removeTab(ui->Tb1->indexOf(new_tab));
     }
 }
 
 
 void MainWindow::on_dm0_clicked()
 {
-    static int flg=0;
-    idx=0;
-    hide_show(idx,flg);
+    static int flg = 0;
+    TopicIdx = 0;
+    hide_show(TopicIdx, flg);
 }
 
 void MainWindow::on_dm1_clicked()
 {
-    static int flg=0;
-    idx=1;
-    hide_show(idx,flg);
+    static int flg = 0;
+    TopicIdx = 1;
+    hide_show(TopicIdx, flg);
 }
 
 void MainWindow::on_dm2_clicked()
 {
-    static int flg=0;
-    idx=2;
-    hide_show(idx,flg);
+    static int flg = 0;
+    TopicIdx = 2;
+    hide_show(TopicIdx, flg);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -20,39 +20,39 @@ QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+	MainWindow(QWidget* parent = nullptr);
+	~MainWindow();
 
 signals:
 private slots:
-    void on_plus_clicked();
-    void on_dm0_clicked();
-    void on_dm1_clicked();
-    void on_dm2_clicked();
-    void connect_subTopic();
-    void on_Tb1_deleteTab(int);
-    void on_Tb1_addTab(int subTopicIdx);
+	void on_plus_clicked();
+	void on_dm0_clicked();
+	void on_dm1_clicked();
+	void on_dm2_clicked();
+	void connect_subTopic();
+	void on_Tb1_deleteTab(int);
+	void on_Tb1_addTab(int subTopicIdx);
 
 private:
-    //UI
-    Ui::MainWindow *ui;
+	//UI
+	Ui::MainWindow* ui;
+
+    int TopicIdx = -1;
 
     //QPLIST
-    int idx=0;
-    int num[3];
-    QPushButton *mb[3];
-    QPushButton* new_button(QString name, QString str);
-    QList<QPushButton*> PBList[3];
-    void hide_show(int idx, int &flg);
+	int num[3];
+	QPushButton* mb[3];
+	QPushButton* new_button(QString name, QString str);
+	QList<QPushButton*> PBList[3];
+	void hide_show(int idx, int& flg);
 
     //TABLIST
-    int TopicIdx=-1;
-    QPushButton* mbt;
-    QList<QWidget*> TList[3];
-    void make_tab(QWidget* Tab);
+	QPushButton* mbt;
+	QList<QWidget*> TList[3];
+	void make_tab(QWidget* Tab);
 
 };
 
