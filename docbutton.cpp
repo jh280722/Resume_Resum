2db9d014312a008eb63310f250fa7e330acfba80
kiuy2 <kiuy2@naver.com>
2020-10-05 21:54:09 +0900

    RR-42 아래꺼에 이어서 커밋

---
+++ docbutton.cpp
@@ -0,0 +1,6 @@
+#include "docbutton.h"
+
+docbutton::docbutton()
+{
+
+}
---
+++ docbutton.h
@@ -0,0 +1,11 @@
+#ifndef DOCBUTTON_H
+#define DOCBUTTON_H
+
+
+class docbutton
+{
+public:
+    docbutton();
+};
+
+#endif // DOCBUTTON_H
